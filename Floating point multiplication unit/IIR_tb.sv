c3a5787807aa89b596ae599d758bff5ff4834502
Gustavo Segura <yo@gustavosegura.com>
2020-08-06 16:20:18 -0600

    filtro  verilog concuerda con octave

--- Floating point multiplication unit/IIR_tb.sv
+++ Floating point multiplication unit/IIR_tb.sv
@@ -1,6 +1,6 @@
 module IIR_tb();
 parameter N_BITS=	32;
-parameter N_SAMPLES=4000;
+parameter N_SAMPLES=1000;
 
  logic clk=0;
  logic reset;
@@ -42,18 +42,17 @@ end
 
 initial begin
 offset_i=32'b00000000000000000000000000000000;
-b1_i=32'b00000000000000000001001010101101;
-b0_i=32'b00000000000000000001001010101101;
-a_i=32'b11111111111111110010010101011100;
-
+b1_i=32'b00000000000000000100101011111010;
+b0_i=32'b00000000000000000100101011111010;
+a_i=32'b11111111111111111001010111110111;
 
 		reset=1;
 		#3;
 		reset=0;
 		#3;
 
-		$readmemb("/home/gsegura/Documents/TEC/2020/analogico/ProyectoFinal/IIR_Verilog/Tools/generado_500.txt", in_ram);
-		f = $fopen("/home/gsegura/Documents/TEC/2020/analogico/ProyectoFinal/IIR_Verilog/Tools/respuesta 500.txt","w");
+		$readmemb("/home/gsegura/Documents/TEC/2020/analogico/ProyectoFinal/IIR_Verilog/Tools/input/MS10KTavo.txt", in_ram);
+		f = $fopen("/home/gsegura/Documents/TEC/2020/analogico/ProyectoFinal/IIR_Verilog/Tools/output/MS10KTavo.txt","w");
       $fwrite(f,"INICIO:\n");
 		#10;
 		
